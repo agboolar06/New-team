228fbd2507fbb1009541da4792189300a8a37818
agboolar06 <agboolar06@gmail.com>
2021-10-14 22:25:02 +0100

    Agboola first commit

---
+++ abdulhakeem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+  
+  cout << "Name:Agboola Abdulhakeem Rotimi\n";
+  cout << "Email:agboolar06@gmail.com\n";
+  cout << "username:agboolar06\n";
+  cout << "Biostack:genomics\n";
+  return 0;
+}
