110169d14d236680fc041361e6eb07aaf6a7550d
Cletus-Hope <polycarphope85@gmail.com>
2021-10-14 21:15:14 +0100

    printname

---
+++ printname.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hope Cletus Okon";
+  cout << "polycarphope85@gmail.com";
+  cout << "Cletus-Hope";
+  cout << "Genomics";
+  return 0;
+}
